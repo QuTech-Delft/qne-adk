ba5dfb09b6f7ce39b91071fc4162ae9c4c6ba61c
Joel Ruhe <ruhejoel@gmail.com>
2021-09-08 14:09:32 +0200

    Added first functionality for structure and valid json string validation

--- src/cli/exceptions.py
+++ src/cli/exceptions.py
@@ -23,13 +23,6 @@ class NotEnoughRoles(Exception):
         super().__init__("The number of roles must be higher than one")
 
 
-class JSONLoadInvalid(Exception):
-    """ Raised when trying to load invalid JSON"""
-
-    def __init__(self, path: str) -> None:
-        super().__init__(f"Trying to load invalid JSON. File trying to load: '{path}'")
-
-
 class InvalidPathName(Exception):
     """ Raised when one of the following characters are used in an input name ['/', '\', '*', ':', '?', '"', '<', '>',
     '|']"""
@@ -37,3 +30,58 @@ class InvalidPathName(Exception):
     def __init__(self, obj: str) -> None:
         super().__init__(f"{obj} name can't contain any of the following characters: ['/', '\\', '*',"
                          f" ':', '?', '\"', '<', '>', '|']")
+
+
+class SchemaValidationError(Exception):
+    """Raised when json file doesn't meet the requirements of the schema"""
+
+    def __init__(self, message="The JSON file you are trying to load does not contain all the required fields"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class JSONValidationError(Exception):
+    """Raised when JSON string is not valid"""
+
+    def __init__(self, message="The JSON file you are trying to load has invalid syntax"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ApplicationDoesntExist(Exception):
+    """Raised when application name doesn't exist in .qne/application.json"""
+
+    def __init__(self, message="Application directory not found. Please make sure you are in your application directory "
+                               "before validating or first create your application"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ApplicationDirectoryNotComplete(Exception):
+    """Raised when the application directory does not contain all the directories and files necessary (MANIFEST, config,
+    and application"""
+    def __init__(self, message="Please make sure your application consists of a MANIFEST.ini, application directory and "
+                               "config directory"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ApplicationConfigNotComplete(Exception):
+    """Raised when the the 'application_name'/config directory is not complete. Should contain the JSON files application,
+    network and result"""
+
+    def __init__(self, message="Are you sure the configuration directory is complete? Should contain the JSON files "
+        "network.json, result.json and application.json"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ApplicationFilesNonExisting(Exception):
+    """Raised when the the 'application_name'/application directory is not complete. Should contain at least one Python
+        file"""
+
+    def __init__(self, message="Are you sure the application directory is complete? Should contain at least one Python "
+                               "file"):
+        self.message = message
+        super().__init__(self.message)
+
---
+++ src/cli/validators.py
@@ -0,0 +1,41 @@
+import json
+import os
+import platform
+
+import jsonschema
+from jsonschema import Draft7Validator, RefResolver, draft7_format_checker
+from cli.exceptions import JSONValidationError
+
+
+def validate_json_string(value):
+    try:
+        _ = json.load(value)
+    except json.decoder.JSONDecodeError:
+        raise JSONValidationError
+
+
+def validate_json_schema(instance, file):
+    # try:
+    with open(file) as json_file:
+        json_schema = json.load(json_file)
+        if platform.system() == 'Windows':
+            path = os.path.dirname(file)
+            json_schema_full_path = os.path.realpath(path).replace('\\', '/')
+            resolver = RefResolver(base_uri=f'file:///{json_schema_full_path}/', referrer=json_schema)
+        else:
+            json_schema_full_path = os.path.realpath(file)
+            resolver = RefResolver(base_uri=f'file://{json_schema_full_path}', referrer=json_schema)
+        Draft7Validator(json_schema, resolver=resolver, format_checker=draft7_format_checker).validate(instance)
+    # except jsonschema.exceptions.ValidationError as ve:
+    #     raise ve.SchemaValidationError({"json_schema_error": f"{'.'.join(str(x) for x in ve.path)}: {ve.message}"}) from ve
+
+
+def validate_json_schema_serializer(instance, serializer):
+    schema_file = getattr(getattr(serializer, 'Meta'), "json_schema", None)
+    if schema_file is not None:
+        validate_json_schema(instance, schema_file)
+
+
+# def validate_case_insensitive_unique(model, field, value):
+#     if model.objects.filter(**{field+"__iexact": value}).count() > 0:
+#         raise DjangoValidationError({field: f"{model.__name__} with this {field} already exists."})
