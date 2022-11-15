bfb7c72a1c4c0dc35a5325873146e1d52c9f45b8
QFer <f.j.grooteman@tudelft.nl>
2022-11-15 11:37:36 +0100

    added python3.10

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering",
     ],
     license="MIT",
