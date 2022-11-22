8a91edddbef00e1cdee0a071957f0136045ccf46
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-22 23:55:29 +0000

    Adding tarfile member sanitization to extractall()

--- src/adk/managers/resource_manager.py
+++ src/adk/managers/resource_manager.py
@@ -77,7 +77,26 @@ class ResourceManager:
                 qne_client.download_source_files(source_file, app_src_path, file_name)
 
             with tarfile.open(file_path, "r:gz") as tar:
-                tar.extractall(app_src_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, app_src_path)
 
             # delete it afterwards
             if file_path.is_file():
